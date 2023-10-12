8c7150eb5184626d994edcc2a221c2f07393b6f6
EremeevTim2004 <eremeevt2004@gmail.com>
2023-10-12 16:18:02 +0300

    update .gitignore

--- Graph.scala
+++ Graph.scala
@@ -1,71 +1,216 @@
 import scala.collection.mutable.{Map, Set}
-import java.io.{File, PrintWriter}
 import scala.io.Source
+import scala.util.{Try, Success, Failure}
+import java.io.{File, PrintWriter}
 
-class Graph[T] {
-  private val adjacencyList: Map[T, Set[T]] = Map()
+class Graph {
+  private var adjacencyList: Map[String, Set[String]] = Map()
 
-  def addVertex(vertex: T): Unit = {
-    if (!adjacencyList.contains(vertex)) {
+  def addVertex(vertex: String): Unit = {
+    if (!adjacencyList.contains(vertex))
       adjacencyList(vertex) = Set()
-    }
   }
 
-  def addEdge(from: T, to: T): Unit = {
-    addVertex(from)
-    addVertex(to)
-    adjacencyList(from) += to
+  def addEdge(source: String, destination: String): Unit = {
+    addVertex(source)
+    addVertex(destination)
+    adjacencyList(source) += destination
+    adjacencyList(destination) += source
   }
 
-  def removeVertex(vertex: T): Unit = {
+  def removeVertex(vertex: String): Unit = {
     if (adjacencyList.contains(vertex)) {
-      adjacencyList.remove(vertex)
-      adjacencyList.foreach { case (_, neighbors) => neighbors -= vertex }
+      val neighbors = adjacencyList(vertex)
+      for (neighbor <- neighbors) {
+        adjacencyList(neighbor) -= vertex
+      }
+      adjacencyList -= vertex
     }
   }
 
-  def removeEdge(from: T, to: T): Unit = {
-    if (adjacencyList.contains(from)) {
-      adjacencyList(from) -= to
-    }
+  def removeEdge(source: String, destination: String): Unit = {
+    adjacencyList(source) -= destination
+    adjacencyList(destination) -= source
   }
 
-  def printToFile(filename: String): Unit = {
+  def saveToFile(filename: String): Unit = {
     val writer = new PrintWriter(new File(filename))
-    try {
-      adjacencyList.foreach {
-        case (vertex, neighbors) =>
-          val neighborsStr = neighbors.mkString(" ")
-          writer.println(s"$vertex: $neighborsStr")
-      }
-    } finally {
-      writer.close()
+    for ((vertex, neighbors) <- adjacencyList) {
+      val neighborsList = neighbors.mkString(", ")
+      writer.println(s"$vertex -> $neighborsList")
+    }
+    writer.close()
+  }
+
+  def display(): Unit = {
+    for ((vertex, neighbors) <- adjacencyList) {
+      val neighborsList = neighbors.mkString(", ")
+      println(s"$vertex -> $neighborsList")
     }
   }
 
-  override def toString: String = {
-    adjacencyList.map {
-      case (vertex, neighbors) => s"$vertex -> ${neighbors.mkString(", ")}"
-    }.mkString("\n")
-  }
-
-  def readFromFile(filename: String): Unit = {
-    val source = Source.fromFile(filename)
-    try {
-      for (line <- source.getLines()) {
-        val parts = line.split(":")
-        if (parts.length == 2) {
-          val vertex = parts(0).trim.asInstanceOf[T]
-          val neighbors = parts(1).trim.split("\\s+").map(_.asInstanceOf[T]).toSet
-          adjacencyList(vertex) = neighbors
-        }
-      }
-    } finally {
-      source.close()
-    }
+  def clearAdjacencyList(): Unit = {
+    adjacencyList = Map()
   }
 }
 
-object Graph {
-  def apply[T](): Graph[T] = new Graph[T]()
+object GraphApp {
+  def main(args: Array[String]): Unit = {
+    var graphs: List[Graph] = List()
+    var currentGraph: Option[Graph] = None
+
+    def printMenu(): Unit = {
+      println(
+        "Menu:\n" +
+        "1. Create a graph\n" +
+        "2. Delete a graph\n" +
+        "3. Create a graph from a configuration file\n" +
+        "4. Configure a graph\n" +
+        "5. Display the adjacency list\n" +
+        "6. Save the configuration to a file\n" +
+        "7. Exit"
+      )
+    }
+
+    def printGraphList(): Unit = {
+      println("List of graphs:")
+      for ((graph, i) <- graphs.zipWithIndex) {
+        println(s"$i: ${if (Some(graph) == currentGraph) "*" else " "}Graph")
+      }
+    }
+
+    var exit = false
+    while (!exit) {
+      printMenu()
+      print("Enter your choice: ")
+      val choice = scala.io.StdIn.readInt()
+      choice match {
+        case 1 => // Create a graph
+          graphs = new Graph() :: graphs
+          currentGraph = Some(graphs.head)
+          println("Graph created.")
+        case 2 => // Delete a graph
+          currentGraph = None
+          if (graphs.nonEmpty) {
+            graphs = graphs.tail
+            println("Graph deleted.")
+          } else {
+            println("No graphs to delete.")
+          }
+        case 3 => // Create a graph from a configuration file
+          println("Enter the filename for the configuration file: ")
+          val filename = scala.io.StdIn.readLine()
+          val newGraph = new Graph()
+          Try {
+            for (line <- Source.fromFile(filename).getLines()) {
+              val parts = line.split("->|\\-")
+              if (parts.length >= 1) {
+                val vertex = parts(0).trim()
+                newGraph.addVertex(vertex)
+                for (i <- 1 until parts.length) {
+                  val neighbor = parts(i).trim()
+                  newGraph.addEdge(vertex, neighbor)
+                }
+              }
+            }
+          } match {
+            case Success(_) =>
+              graphs = newGraph :: graphs
+              currentGraph = Some(graphs.head)
+              println("Graph created from the configuration file.")
+            case Failure(ex) =>
+              println(s"Error reading the configuration file: ${ex.getMessage}")
+          }
+        case 4 => // Configure a graph
+          if (graphs.nonEmpty) {
+            println("Enter the index of the graph to configure (0 to ${graphs.length - 1}): ")
+            val index = scala.io.StdIn.readInt()
+            if (index >= 0 && index < graphs.length) {
+              currentGraph = Some(graphs(index))
+              configureGraph(currentGraph.get)
+            } else {
+              println("Invalid index.")
+            }
+          } else {
+            println("No graphs to configure.")
+          }
+        case 5 => // Display the adjacency list
+          currentGraph.foreach { graph =>
+            println("Adjacency list:")
+            graph.display()
+          }
+        case 6 => // Save the configuration to a file
+          currentGraph.foreach { graph =>
+            println("Enter the filename to save the configuration: ")
+            val filename = scala.io.StdIn.readLine()
+            graph.saveToFile(filename)
+            println(s"Graph configuration saved to $filename.")
+          }
+        case 7 => // Exit
+          exit = true
+        case _ =>
+          println("Invalid choice. Please select a valid option.")
+      }
+    }
+  }
+
+  def configureGraph(graph: Graph): Unit = {
+    var exit = false
+    while (!exit) {
+      println(
+        "Graph Configuration Menu:\n" +
+        "1. Add a vertex\n" +
+        "2. Remove a vertex\n" +
+        "3. Add an edge\n" +
+        "4. Remove an edge\n" +
+        "5. Copy graph to a configuration file\n" +
+        "6. Display adjacency list\n" +
+        "7. Clear adjacency list\n" +
+        "8. Exit"
+      )
+      print("Enter your choice: ")
+      val choice = scala.io.StdIn.readInt()
+      choice match {
+        case 1 => // Add a vertex
+          println("Enter the vertex name: ")
+          val vertex = scala.io.StdIn.readLine()
+          graph.addVertex(vertex)
+          println(s"Vertex $vertex added.")
+        case 2 => // Remove a vertex
+          println("Enter the vertex name to remove: ")
+          val vertex = scala.io.StdIn.readLine()
+          graph.removeVertex(vertex)
+          println(s"Vertex $vertex removed.")
+        case 3 => // Add an edge
+          println("Enter the source vertex: ")
+          val source = scala.io.StdIn.readLine()
+          println("Enter the destination vertex: ")
+          val destination = scala.io.StdIn.readLine()
+          graph.addEdge(source, destination)
+          println(s"Edge from $source to $destination added.")
+        case 4 => // Remove an edge
+          println("Enter the source vertex: ")
+          val source = scala.io.StdIn.readLine()
+          println("Enter the destination vertex: ")
+          val destination = scala.io.StdIn.readLine()
+          graph.removeEdge(source, destination)
+          println(s"Edge from $source to $destination removed.")
+        case 5 => // Copy graph to a configuration file
+          println("Enter the filename to save the configuration: ")
+          val filename = scala.io.StdIn.readLine()
+          graph.saveToFile(filename)
+          println(s"Graph configuration saved to $filename.")
+        case 6 => // Display adjacency list
+          println("Adjacency list:")
+          graph.display()
+        case 7 => // Clear adjacency list
+          graph.clearAdjacencyList()
+          println("Adjacency list cleared.")
+        case 8 => // Exit
+          exit = true
+        case _ =>
+          println("Invalid choice. Please select a valid option.")
+      }
+    }
+  }
 }
